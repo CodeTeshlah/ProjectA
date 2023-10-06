58d5430ee74f5564549e133deb6fdb5b717ee57b
CodeTeshlah <146417993+CodeTeshlah@users.noreply.github.com>
2023-10-06 13:05:58 +0800

    updated left and right movement

--- fayr/galawgalaw.gd
+++ fayr/galawgalaw.gd
@@ -4,13 +4,17 @@ extends Node3D
 @onready var path1 = $Path3D#/PathFollow3D
 @onready var path2 = $Path3DLeft#/PathFollow3D
 @onready var path3 = $Path3DRight#/PathFollow3D
+var running = 1
 func _ready():
 	pass # Replace with function body.
 
 func _process(delta):
 		const move_speed = 10.0
-		#if (check_input()):
-		path.progress += move_speed * delta
+		if (check_input()):
+			running = 2
+		else:
+			running = 1
+		path.progress += (move_speed * running ) * delta
 		print(path.progress_ratio >= 1)
 		if (path.progress_ratio) >= 1:
 			check_direction()
@@ -26,9 +30,9 @@ func check_input():
 func check_direction():
 	if (Input.is_action_just_pressed('player_move_left')):
 		path1.remove_child(player)
-		path2.add_child(player)
+		path3.add_child(player)
 		path.progress = 0
 	if (Input.is_action_just_pressed("player_move_right")):
 		path1.remove_child(player)
-		path3.add_child(player)
+		path2.add_child(player)
 		path.progress = 0
